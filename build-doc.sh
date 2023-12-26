74c4bd98b7aa68fd13060e37122b43d226d3b320
Saheb Roy <saheb.roy@sixt.com>
2023-12-26 16:57:48 +0100

    added shell script

---
+++ build-doc.sh
@@ -0,0 +1,12 @@
+cd DocCPublisher
+xcrun xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO docbuild \
+    -scheme DocCPublisherDemo \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath "$PWD/.derivedData"
+
+xcrun docc process-archive transform-for-static-hosting \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/DocCPublisherDemo.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "DocCDemo"
+    
+echo '<script>window.location.href += "/documentation/doccpublisherdemo"</script>' > .docs/index.html
