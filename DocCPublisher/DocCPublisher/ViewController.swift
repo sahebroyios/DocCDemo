b4cefec411da6a77a587ccef5cc6e5e002234978
Saheb Roy <saheb.roy@sixt.com>
2023-12-26 16:53:50 +0100

    First commit of source code

---
+++ DocCPublisher/DocCPublisher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocCPublisher
+//
+//  Created by Saheb Roy on 25.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
