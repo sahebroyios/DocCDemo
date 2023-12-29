9289cee4f74484f1c36ea1c1eba0963494708c51
Saheb Roy <saheb.roy@sixt.com>
2023-12-29 15:08:36 +0100

    Updated

--- DocCPublisher/DocCPublisher/DocCTextField.swift
+++ DocCPublisher/DocCPublisher/DocCTextField.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 /**
- An HoshiTextField is a subclass of the TextFieldEffects object, is a control that displays an UITextField with a customizable visual effect around the lower edge of the control.
+ An DocCTextField is a subclass of the TextFieldEffects object, is a control that displays an UITextField with a customizable visual effect around the lower edge of the control.
  */
+
 @IBDesignable open class DocCTextField: TextFieldEffects {
     /**
      The color of the border when it has no content.
